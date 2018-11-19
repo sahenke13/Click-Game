b64cf44629236db336cac89263384bd76ae73c0a
sahenke13 <shenke13@gmail.com>
2018-11-19 16:20:41 -0600

    Everything Works

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -13,7 +13,50 @@ class App extends React.Component {
   state = {
     score: 0,
     highScore: 0,
-    chars: charList
+    charList
+  }
+
+  // where in is current index, and j represents next index.  Fisher/Yates Array Shuffle method
+shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+IncrementScore = () => {
+  const newScore = this.state.score +1;
+  this.setState({score: newScore})
+}
+
+
+
+//click function.
+clicked = (id) => {
+     
+    const currentChar = this.state.charList.filter(chars=>{
+        return (chars.id === id)
+      })[0];
+     if(currentChar.clicked === false){
+       this.IncrementScore();
+       currentChar.clicked = true;
+       this.shuffleArray(this.state.charList);
+     }
+     else{
+        alert("You Lose. Try Again")
+       console.log("Game Over");
+       if(this.state.score > this.state.highScore){
+         this.setState({highScore: this.state.score,
+                          score: 0})
+       }
+       this.state.charList.forEach(char => (char.clicked = false))
+     }
+      console.log(currentChar);
+
+   
+   
+    
   }
 
   render()  {
@@ -23,12 +66,14 @@ class App extends React.Component {
         <HeaderCard score = {this.state.score} highScore = {this.state.highScore}>SuperHero Click Game</HeaderCard>
        <div id = "charWell">
         
-        {this.state.chars.map(char =>(
+        {this.state.charList.map(char =>(
           <CharCard 
+            key = {char.id}
             id= {char.id}
             image={char.image}
             clicked={char.clicked}
             name={char.name}
+            clickChar ={this.clicked}
             />
         ))
         }
--- click-game/src/components/CharacterCard/CharacterCard.js
+++ click-game/src/components/CharacterCard/CharacterCard.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./CharacterCard.css";
 
 const CharCard = props =>(
-    <div className = "card" >
-        <div className = "img-container" >
+    <div className = "card"  onClick = {() => props.clickChar(props.id)}>
+        <div className = "img-container">
             <img src = {props.image} alt = {props.name} height = "150px" width = "auto" key = {props.id}/>
         </div>
     </div>
